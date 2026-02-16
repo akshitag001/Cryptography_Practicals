abc58818f23cd2487e1c8038d9578905d5454688
akshitag001 <205055593+akshitag001@users.noreply.github.com>
2026-02-16 15:56:21 +0530

    Update fiestel_cipher.c

--- fiestel_cipher.c
+++ fiestel_cipher.c
@@ -1,62 +1,63 @@
-#include<stdio.h>
-#include<stdint.h>  
-//stdint.h is used for fixed width integers , cause crypto must use exact sizes , uint32_t,uint64_t
+#include <stdio.h>
+
+#define ull unsigned long long
+#define ul  unsigned long
+#define PP  (((ull)1 << 32) - 1)
 
 
-uint32_t F(uint32_t right, uint32_t key){
-  uint32_t x = right ^ key;
-return(x<<1) | (x >> 31);
+ul F(ul r, ul k)
+{
+    return (r ^ k);
 }
 
-uint64_t fiestel_encrypt(uint64_t plaintext, uint32_t keys[],int rounds){
-  uint32_t left =(uint32_t)(plaintext >> 32);
-  uint32_t right = (uint32_t)(plaintext << 64);
+int main()
+{
+    ull p, c;        // plaintext, ciphertext
+    ul l0, r0;       // initial halves
+    ul l1, r1;       // after one round
+    ul k;            // key
 
-  for(int i = 0;i< rounds;i++){
-     uint32_t temp = right;
-     right = left ^ F(right, key[i]);
-     left= temp;
-  }
-return ((uint64_t)left << 32) | right;
-}
+    printf("Enter plaintext (hex, without 0x): ");
+    scanf("%llx", &p);
 
-uint64_t feistel_decrypt(uint64_t ciphertext,uint32_t key[],int rounds) {
-  uint32_t left = (uint32_t)(ciphertext >> 32); 
-  uint32_t right = (uint32_t)(ciphertext && 0xFFFFFFFF);
- for(int i = rounds-1; i>=0; i--){
-    uint32_t temp = left;
-    left = right ^ F(left,keys[i]);
-    right = temp;
- }
-return ((uint64_t)left << 32) | right;
-}
+    printf("Enter key (hex): ");
+    scanf("%lx", &k);
 
-int main() {
-  uint64_t input;
-  int choice;
-  uint32_t keys[8] = {
-      0xA56BABCD, 0x000FF111, 0xABCDEFFF, 0x12345678,
-        0x87654321, 0xF0F0F0F0, 0x11111111, 0x22222222
-    };
+    
+    l0 = p >> 32;
+   
+    r0 = p & PP;
+   
 
-printf("   64 bit Fiestel Cipher   \n");
-printf("Enter 64 bit data (hex , without 0x): ");
-scanf("%lx", &input);
-pritnf("\n1. Encrypt\n2. Decrypt\n Choose Option:
-    scanf("%d", &choice);
+  
+  
+    l1 = r0;
+    r1 = l0 ^ F(r0, k);
+    printf("l1: 0x%llX\n", l1);
+    printf("r1: 0x%llX\n", r1);
 
-    if (choice == 1) {
-        uint64_t cipher = feistel_encrypt(input, keys, 8);
-        printf("Ciphertext: 0x%lX\n", cipher);
-    }
-    else if (choice == 2) {
-        uint64_t plain = feistel_decrypt(input, keys, 8);
-        printf("Plaintext:  0x%lX\n", plain);
-    }
-    else {
-        printf("Invalid option.\n");
-    }
+    c = 0;
+    c = c|l1;
+    c = (c << 32) | r1;
+
+    printf("Ciphertext: 0x%llX\n", c);
+    
+    
+    l1 = c >> 32;
+    r1 = c & PP;
+
+    
+    r0 = l1;
+    l0 = r1 ^ F(l1, k);
+     printf("l0: 0x%llX\n", l0);
+     printf("r0: 0x%llX\n", r0);
+
+    
+    p = 0;
+    p = p|l0;
+    p = (p << 32) | r0;
+
+    printf("Recovered Plaintext: 0x%llX\n", p);
 
     return 0;
 }
-
